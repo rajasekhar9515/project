5205312823534b3cf5dd36c41734e92cca346a0a
rajasekhar <rajasekhar123@gmail.com>
2020-01-21 13:02:37 +0530

    1st

---
+++ src/test/java/project/patterntest.java
@@ -0,0 +1,14 @@
+package project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class patterntest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
